fbbf06b3c02e36d89920d4f5c11d97a3db1f2431
rahulvikhe1 <rahulvikhe1@gmail.com>
2023-10-12 00:19:13 +0530

    update SEO properties

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Rahul Vikhe',
     description:
       'Rahul is a cloud enthusiast, with a specialization in creating and optimizing outstanding digital solutions.',
-    siteUrl: 'https://rahulvikhe.cloud', // No trailing slash allowed!
+    siteUrl: 'https://rahulvikhe.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@rahulvikhe05',
   },
