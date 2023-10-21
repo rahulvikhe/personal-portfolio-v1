5d72ed306dda5519f454da85735691a950752b37
rahulvikhe1 <rahulvikhe1@gmail.com>
2023-10-21 21:44:50 +0530

    Change variables.js

--- src/styles/variables.js
+++ src/styles/variables.js
@@ -12,7 +12,7 @@ const variables = css`
     --light-slate: #a8b2d1;
     --lightest-slate: #ccd6f6;
     --white: #e6f1ff;
-    --green: #ffb703;
+    --green: #64ffda;
     --green-tint: rgba(100, 255, 218, 0.1);
     --pink: #f57dff;
     --blue: #57cbff;
